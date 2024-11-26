a38c95db24bccb73129aea9828adaafab03d9f71
HubLip <01171666@pw.edu.pl>
2024-11-26 23:12:00 +0100

    zejsciowka6.1

--- foo.hpp
+++ foo.hpp
@@ -6,11 +6,26 @@
 #include <list>
 #include <vector>
 
+using namespace std;
+
 std::vector< char > foo(std::list< Human >& people)
 {
-    std::vector< char > retval(people.size());
+    vector< char > retval(people.size()); //tworzymy wektor o rozmiarze people
 
-    // Twoja implementacja tutaj
+    auto lam = [](Human& person) { person.birthday(); };
+    for_each(people.begin(), people.end(), lam); //wykonujemy na kazdym elemencie lam czyli wywoluje metode birthday dla kazdego skladnika w wektorze. Uzywamy Human& bo przekazujemy oryginalny obiekt, a nie kopie
+
+    auto lambda = [](Human& person) 
+    { 
+        char result = 'y';
+        if (person.isMonster()) 
+        {
+            result = 'n';
+        }
+        return result;
+    };
+    // Tworzenie wektora zwracanego w odwrotnej kolejności i sprawdzanie warunku isMonster
+    transform(people.rbegin(), people.rend(), retval.begin() ,lambda);
 
     return retval;
 }
